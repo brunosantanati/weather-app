5715e5590182c15a1ace3a20e54e091e5c8e438d
Bruno Rafael Sant' Ana <bruno.santana.ti@gmail.com>
2020-03-24 01:15:38 -0300

    Customizing HTTP request using query string and enable automatically json parse.

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const request = require('request')
 
-const url = 'https://api.darksky.net/forecast/70ff7c58595674b62c6cb99468310588/37.8267,-122.4233'
+const url = 'https://api.darksky.net/forecast/70ff7c58595674b62c6cb99468310588/-23.633690,-46.472490?units=si'
+//Parameters for request:
+//units=si or units=us
+//lang=es
 
-request({ url: url }, (error, response) => {
-    //console.log(response)
-    const data = JSON.parse(response.body)
-    //console.log(data)
-    console.log(data.currently)
+request({ url: url, json: true }, (error, response) => {
+    //console.log(response.body.currently)
+    console.log(response.body.daily.data[0].summary + ' It is currently ' + response.body.currently.temperature + ' degrees out. There is a ' + response.body.currently.precipProbability + '% chance of rain.')
 })
