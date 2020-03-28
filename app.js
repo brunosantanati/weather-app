ff8d96d6d7814a74fe8e8bda34ee18ce6bf995f3
Bruno Rafael Sant' Ana <bruno.santana.ti@gmail.com>
2020-03-27 23:28:40 -0300

    Implement reusable code for getting latitude and longitude from address

--- app.js
+++ app.js
@@ -1,30 +1,21 @@
-const request = require('request')
+const geocode = require('./uitls/geocode')
 
-const url = 'https://api.darksky.net/forecast/70ff7c58595674b62c6cb99468310588/-23.633690,-46.472490?units=si'
-//Parameters for request:
-//units=si or units=us
-//lang=es
+// const url = 'https://api.darksky.net/forecast/70ff7c58595674b62c6cb99468310588/-23.633690,-46.472490?units=si'
+// //Parameters for request:
+// //units=si or units=us
+// //lang=es
 
-request({ url: url, json: true }, (error, response) => {
-    if(error){
-        console.log('Unable to connect to weather service!')
-    }else if(response.body.error){
-        console.log('Unable to find location!')
-    }else{
-        console.log(response.body.daily.data[0].summary + ' It is currently ' + response.body.currently.temperature + ' degrees out. There is a ' + response.body.currently.precipProbability + '% chance of rain.')
-    }
-})
+// request({ url: url, json: true }, (error, response) => {
+//     if(error){
+//         console.log('Unable to connect to weather service!')
+//     }else if(response.body.error){
+//         console.log('Unable to find location!')
+//     }else{
+//         console.log(response.body.daily.data[0].summary + ' It is currently ' + response.body.currently.temperature + ' degrees out. There is a ' + response.body.currently.precipProbability + '% chance of rain.')
+//     }
+// })
 
-const geocodeUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiYnJ1bm9zYW50YW5hdGkiLCJhIjoiY2s4ODFmMHB4MDBkeDNnbXNxOHhqYjBjaiJ9.mj3Dg_SMDKGbiOJ2oyT4Cw&limit=1'
-
-request({ url: geocodeUrl, json: true}, (error, response) => {
-    if(error){
-        console.log('Unable to connect to location service!')
-    }else if(response.body.features.length === 0){
-        console.log('Unable to find location. Try another search.')
-    }else{
-        const latitude = response.body.features[0].center[1];
-        const longitude = response.body.features[0].center[0];
-        console.log(latitude, longitude)
-    }
+geocode('Pq. São Rafael, São Paulo', (error, data) => {
+    console.log('Error', error)
+    console.log('Data', data)
 })
---
+++ uitls/geocode.js
@@ -0,0 +1,21 @@
+const request = require('request')
+
+const geocode = (address, callback) => {
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoiYnJ1bm9zYW50YW5hdGkiLCJhIjoiY2s4ODFmMHB4MDBkeDNnbXNxOHhqYjBjaiJ9.mj3Dg_SMDKGbiOJ2oyT4Cw&limit=1'
+
+    request({ url: url, json: true}, (error, response) => {
+        if(error){
+            callback('Unable to connect to location service!', undefined)
+        }else if(response.body.features.length === 0){
+            callback('Unable to find location. Try another search.', undefined)
+        }else{
+            callback(undefined, {
+                latitude: response.body.features[0].center[1],
+                longitude: response.body.features[0].center[0],
+                location: response.body.features[0].place_name
+            })
+        }
+    })
+}
+
+module.exports = geocode
