49b2b332e9a916b5aa8ba060d2d35fd5a43870c0
Bruno Rafael Sant' Ana <bruno.santana.ti@gmail.com>
2020-03-29 21:55:25 -0300

    Translate messages to pt.

--- uitls/forecast.js
+++ uitls/forecast.js
@@ -2,7 +2,7 @@ const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
 
-    const url = 'https://api.darksky.net/forecast/70ff7c58595674b62c6cb99468310588/' + latitude + ',' + longitude + '?units=si'
+    const url = 'https://api.darksky.net/forecast/70ff7c58595674b62c6cb99468310588/' + latitude + ',' + longitude + '?units=si&lang=pt'
 
     request({ url, json: true }, (error, { body } ) => {
         if(error){
@@ -10,7 +10,7 @@ const forecast = (latitude, longitude, callback) => {
         }else if(body.error){
             callback('Unable to find location!', undefined)
         }else{
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' Temperatura atual: ' + body.currently.temperature + ' graus. Existe ' + body.currently.precipProbability + '% chances de chover.')
         }
     })
 
